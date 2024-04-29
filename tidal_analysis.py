85b6ff3e3bfdd063ae7c6872be22ab9bdcadf8f5
Isaac Abbott <ia947@york.ac.uk>
2024-04-29 21:29:30 +0100

    Create tidal_data function under read_tidal_data, missing .mask

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -8,22 +8,29 @@ import uptide
 import pytz
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
+import glob
 
-# Create tide_data function
+# Create tidal_data function
 def read_tidal_data(filename):
+    tidal_data = pd.read_table(filename, skiprows = 11, names=["Cycle", "Date", "Time", "Sea Level", "Residual"], delimiter = r"\s+")
+    tidal_data['Date'] = pd.to_datetime(dict(year=tidal_data[0]), month=tidal_data[1], day=tidal_data[2], hour=tidal_data[3])
+    tidal_data = tidal_data.drop(['Cycle', 'Residual'], axis='columns')
+    tidal_data = tidal_data.set_index('Date')
+    #tidal_data = tidal_data.mask(tidal_data['Sea Level'] == -300)
 
-    return 0
+    return tidal_data
     
 # Create year_data function
 def extract_single_year_remove_mean(year, data):
-   year_string_start = str(year)+"0101" # (Remove comment) January 1st
-   year_string_end = str(year)+"1231" # (Remove comment) December 31st
-   year_data = data.loc[year_string_start:year_string_end, ['Tide']]
-   # Remove mean to oscillate around zero
-   mmm = np.mean(year_data['Tide'])
-   year_data['Tide'] -=mmm
+    year_string_start = str(year)+"0101" # (Remove comment) January 1st
+    year_string_end = str(year)+"1231" # (Remove comment) December 31st
+    year_data = data.loc[year_string_start:year_string_end, ['Tide']]
+    # Remove mean to oscillate around zero
+    mmm = np.mean(year_data['Tide'])
+    year_data['Tide'] -=mmm
 
-   return year_data
+    return year_data
 
 
 def extract_section_remove_mean(start, end, data):
@@ -31,7 +38,7 @@ def extract_section_remove_mean(start, end, data):
 
     return 
 
-
+#create loop here<<<<<<<<<<
 def join_data(data1, data2):
 
     return 
@@ -56,10 +63,10 @@ def get_longest_contiguous_data(data):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-                     prog="UK Tidal analysis",
-                     description="Calculate tidal constiuents and RSL from tide gauge data",
-                     epilog="Copyright 2024, Jon Hill"
-                     )
+                      prog="UK Tidal analysis",
+                      description="Calculate tidal constiuents and RSL from tide gauge data",
+                      epilog="Copyright 2024, Jon Hill"
+                      )
 
     parser.add_argument("directory",
                     help="the directory containing txt files with data")
