4411ac363b2df398786b6fc0607d41fa3f4de664
Isaac Abbott <ia947@york.ac.uk>
2024-04-30 16:17:53 +0100

    Add comments to each block, finish .mask, change import order

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -2,22 +2,23 @@
 import datetime
 import os
 import math
+import csv
+import glob
+import argparse
 import wget
 import numpy as np
 import uptide
 import pytz
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
-import glob
 
 # Create tidal_data function
 def read_tidal_data(filename):
     tidal_data = pd.read_table(filename, skiprows = 11, names=["Cycle", "Date", "Time", "Sea Level", "Residual"], delimiter = r"\s+")
-    tidal_data['Date'] = pd.to_datetime(dict(year=tidal_data[0]), month=tidal_data[1], day=tidal_data[2], hour=tidal_data[3])
+    tidal_data['Date'] = pd.to_datetime(dict(year=tidal_data[0], month=tidal_data[1], day=tidal_data[2], hour=tidal_data[3]))
     tidal_data = tidal_data.drop(['Cycle', 'Residual'], axis='columns')
     tidal_data = tidal_data.set_index('Date')
-    #tidal_data = tidal_data.mask(tidal_data['Sea Level'] == -300)
+    tidal_data = tidal_data.mask(tidal_data['Sea Level'] < -300)
 
     return tidal_data
     
@@ -32,29 +33,31 @@ def extract_single_year_remove_mean(year, data):
 
     return year_data
 
-
+# Create functions for each area: Aberdeen_data, Dover_data, Whitby_data
 def extract_section_remove_mean(start, end, data):
-
+    
 
     return 
 
-#create loop here<<<<<<<<<<
+# Create complete_data function with a loop joining all location files
 def join_data(data1, data2):
 
     return 
 
 
-
+# Calculate the rate of SLR for each location
 def sea_level_rise(data):
 
                                                      
     return 
 
+# Calculate M2 and S2 tidal components for each station
 def tidal_analysis(data, constituents, start_datetime):
 
 
     return 
 
+# Create function to show longest continuous segment of data???
 def get_longest_contiguous_data(data):
 
 
@@ -79,5 +82,3 @@ if __name__ == '__main__':
     dirname = args.directory
     verbose = args.verbose
     
-
-
