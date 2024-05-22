b6a0819297d46d17007396bad6f3729d4fa9d76a
Isaac Abbott <ia947@york.ac.uk>
2024-05-22 18:25:52 +0100

    Refine the sea_level_rise function, change data variable names to not duplicate, add module and function docstrings

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -1,11 +1,14 @@
+'''
+This module performs tidal analysis including reading, joining,
+analysing sea level data from guages, and performing tidal harmonic
+analysis using uptide
+'''
+
 # Import required modules
-#import datetime
-#import os
-import math
-#import csv
-#import glob
+import datetime
 import argparse
-#import wget
+import glob
+import os
 import numpy as np
 import uptide
 import pytz
@@ -13,12 +16,9 @@ import pandas as pd
 from matplotlib import dates
 from scipy.stats import linregress
 
-#file_paths = glob.glob('data/aberdeen/*.txt') + glob.glob('data/dover/*.txt') + glob.glob('data/whitby/*.txt')
-#file_paths = glob.glob('data/*.txt')
-filename = ['data/1946ABE.txt', 'data/1947ABE.txt']
-
-# Create read_tidal_data function
 def read_tidal_data(filename):
+    ''' Create read_tidal_data function to take a .txt input and format the correct Datetime,
+        assert an appropriate name for the Sea Level column, and replace corrupted values with NaN '''
     tidal_data = pd.read_csv(filename, skiprows = 11, names=["Cycle", "Date", "Time", "Sea Level", "Residual"], delimiter = r"\s+")
     # Combine 'Date' and 'Time' columns into 'Datetime' column
     tidal_data['Datetime'] = pd.to_datetime(tidal_data['Date'] + ' ' + tidal_data['Time'], format="%Y/%m/%d %H:%M:%S")
@@ -30,59 +30,57 @@ def read_tidal_data(filename):
     tidal_data['Sea Level'] = tidal_data['Sea Level'].astype(float)
     return tidal_data
 
-# Create join_data function with a loop joining all location files. Sort data into chronological order.
 def join_data(data1, data2):
+    ''' Create join_data function to concat data into a new dataframe,
+        then sort into chronological order '''
     # Join data into a new dataframe
     join = pd.concat([data1, data2])
-    # Sort values into chronological order
+    # Sort chronologically
     join.sort_index(inplace=True)
     return join
 
-# Create year_data function
 def extract_single_year_remove_mean(year, data):
+    ''' Create function to only read data for a given year, removing mean so
+        tidal values oscillate around zero '''
     year_string_start = str(year)+"0101"
     year_string_end = str(year)+"1231"
     # Read data between Jan 1 and Dec 31
     year_data = data.loc[year_string_start:year_string_end, ['Sea Level']]
-    year_mean = np.mean(year_data['Sea Level'])
     # Remove year mean
-    year_data['Sea Level'] -= year_mean
+    year_data['Sea Level'] -= np.mean(year_data['Sea Level'])
     return year_data
 
-# Create section_data function
 def extract_section_remove_mean(start, end, data):
+    ''' Create function to only read data for a given section, removing mean so
+        tidal values oscillate around zero '''
     section_start = str(start)
     section_end = str(end)
     # Read data in the given section
     section_data = data.loc[section_start:section_end, ['Sea Level']]
-    section_mean = np.mean(section_data['Sea Level'])
     # Remove section mean
-    section_data['Sea Level'] -= section_mean
+    section_data['Sea Level'] -= np.mean(section_data['Sea Level'])
     return section_data
 
-# Calculate the rate of SLR for each location
-def sea_level_rise(data):
+def sea_level_rise(sea_level_data):
+    ''' Calculate the rate of SLR using scipy linregress '''
     # Remove NaN values
-    data = data.dropna()
+    sea_level_data = sea_level_data.dropna()
     # Remove mean value
-    data_mean = np.mean(data['Sea Level'])
-    data['Sea Level'] -= data_mean
+    sea_level_data['Sea Level'] -= np.mean(sea_level_data['Sea Level'])
     # Turn the index from 'dates2num'
-    data.index = dates.date2num(data.index)
+    sea_level_data.index = dates.date2num(sea_level_data.index)
     # Calculate SLR using linear regression
-    slope, intercept, r_value, p_value, std_err = linregress(data.index, data['Sea Level'])
+    slope, intercept, r_value, p_value, std_err = linregress(sea_level_data.index, sea_level_data['Sea Level'])
     return slope, p_value
 
-# Calculate M2 and S2 tidal components for each station
-def tidal_analysis(data, constituents, start_datetime):
+def tidal_analysis(tidal_data, constituents, start_datetime):
     
     return 
 
-# Create function to show longest continuous segment of data??? using uptide
 def get_longest_contiguous_data(data):
     
-    return 
-# Re-visit recording from 9/5/24
+    return
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
@@ -101,4 +99,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     dirname = args.directory
     verbose = args.verbose
-    
+
